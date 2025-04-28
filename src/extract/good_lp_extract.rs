b0e7f8a11a4846d060a628e626a7e468c4d81c00
Neel Patel <neelpatelbiz@gmail.com>
2025-04-28 15:13:11 -0400

    create topo vars

--- src/extract/good_lp_extract.rs
+++ src/extract/good_lp_extract.rs
@@ -15,6 +15,16 @@ pub struct GoodLPExtractor;
 impl Extractor for GoodLPExtractor {
     fn extract(&self, egraph: &EGraph, roots: &[ClassId]) ->
     ExtractionResult {
+        let mut vars = variables!();
+
+        /* t_m */
+        const EPS: f64 = 1e-3;
+        const ALPHA: f64 = 2.0;
+        let mut topo_vars: HashMap<ClassId, Variable> = HashMap::new();
+        for (class_id, class) in egraph.classes() {
+            let t_m = vars.add(variable().min(0.0).max(1.0));
+            topo_vars.insert(class_id.clone(), t_m);
+        }
 
         for (class_id,class) in egraph.classes() {
             print!("class {:?}: ", class.id);
